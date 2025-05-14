ae692e2eeac2672f60e9bc4f5b72ebd749384777
Amrik-Bhadra <amrik.bhadra@gmail.com>
2025-05-14 22:37:18 +0530

    scrollbar edited

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import scrollbarplugin from 'tailwind-scrollbar';
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html",
@@ -15,5 +17,5 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [scrollbarplugin],
 }
