f09d2aa9de69942dcb3b7c1571d02ff3d952550a
Amrik-Bhadra <amrik.bhadra@gmail.com>
2025-05-16 12:33:25 +0530

    noscrollbar added

--- src/components/skills_component.jsx/SkillsCard.jsx
+++ src/components/skills_component.jsx/SkillsCard.jsx
@@ -4,7 +4,7 @@ const SkillsCard = ({ key, img, title }) => {
   return (
     <div
       key={key}
-      className="backdrop-blur-md bg-[#161B22] border border-gray-700 rounded-xl shadow-lg p-4 flex flex-col items-center justify-center transition-transform hover:scale-105 duration-300 h-28 w-28"
+      className="backdrop-blur-md bg-[#161B22] border border-gray-700 rounded-xl shadow-lg p-4 flex flex-col items-center justify-center transition-transform hover:scale-105 duration-300 h-28 w-28 shrink-0"
     >
       <img
         src={img}
--- src/components/skills_component.jsx/SkillsDiv.jsx
+++ src/components/skills_component.jsx/SkillsDiv.jsx
@@ -5,7 +5,7 @@ const SkillsDiv = ({title, skillsList}) => {
   return (
     <div className="p-3 flex flex-col gap-y-3">
       <h3 className="text-[#8AB4F8] text-xl font-semibold capitalize">{title}</h3>
-      <div className="p-3 flex gap-x-4">
+      <div className="p-3 flex gap-x-4 overflow-x-auto no-scrollbar">
         {
             skillsList.map((skill, index)=>(
                 <SkillsCard key={index} img={skill.image} title={skill.name} />
