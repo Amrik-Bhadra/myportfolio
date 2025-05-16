e6b1854b60411978aa3f88849626f9fae51235e4
Amrik-Bhadra <amrik.bhadra@gmail.com>
2025-05-16 13:14:56 +0530

    made the footer responsive

--- src/pages/subsections/FooterSection.jsx
+++ src/pages/subsections/FooterSection.jsx
@@ -4,20 +4,20 @@ import { FaPhoneAlt } from "react-icons/fa";
 
 const FooterSection = () => {
   return (
-    <div className="flex items-center justify-center bg-[#161B22] shadow-md border-2 border-gray-700">
-      <div className="w-[85%] py-3 flex justify-between items-center">
-        <span className="flex flex-col gap-y-1">
+    <div className="py-3 md:py-1 flex items-center justify-center bg-[#161B22] shadow-md border-2 border-gray-700">
+      <div className="w-[85%] py-3 flex flex-col gap-y-20 md:flex-row justify-between items-center">
+        <span className="flex flex-col gap-y-1 items-center md:items-start">
             <h2 className="text-[#eee] font-semibold text-xl mb-2">Amrik Bhadra</h2>
             <p className="flex items-center gap-x-2 text-[#8AB4F8] text-base"> <MdEmail/> amrik.bhadra@gmail.com</p>
             <p className="flex items-center gap-x-2 text-[#8AB4F8] text-base"><FaPhoneAlt/> +91-7739226540</p>
         </span>
         
-        <div className="flex gap-x-30">
-          <div>
+        <div className="grid grid-cols-2 md:flex md:flex-row gap-y-10 md:gap-x-30">
+          <div className="flex flex-col items-center">
             <h4 className="text-[#8AB4F8] font-semibold mb-2 uppercase">
               About
             </h4>
-            <ul className="text-sm text-[#ccc] flex flex-col gap-y-1">
+            <ul className="text-sm text-[#ccc] gap-y-1 flex flex-col items-center">
               <li>
                 <a href="#about">Education</a>
               </li>
@@ -33,11 +33,11 @@ const FooterSection = () => {
             </ul>
           </div>
 
-          <div>
+          <div className="flex flex-col items-center">
             <h4 className="text-[#8AB4F8] font-semibold mb-2 uppercase">
               Skills
             </h4>
-            <ul className="text-sm text-[#ccc] flex flex-col gap-y-1">
+            <ul className="text-sm text-[#ccc] flex flex-col gap-y-1 items-center">
               <li>
                 <a href="#skills">Frontend</a>
               </li>
@@ -56,11 +56,11 @@ const FooterSection = () => {
             </ul>
           </div>
 
-          <div>
+          <div className="flex flex-col items-center">
             <h4 className="text-[#8AB4F8] font-semibold mb-2 uppercase">
               Showcase
             </h4>
-            <ul className="text-sm text-[#ccc] flex flex-col gap-y-1">
+            <ul className="text-sm text-[#ccc] flex flex-col gap-y-1 items-center">
               <li>
                 <a href="#">Projects</a>
               </li>
@@ -70,11 +70,11 @@ const FooterSection = () => {
             </ul>
           </div>
 
-          <div>
+          <div className="flex flex-col items-center">
             <h4 className="text-[#8AB4F8] font-semibold mb-2 uppercase">
               Credits
             </h4>
-            <ul className="text-sm text-[#ccc] flex flex-col gap-y-1">
+            <ul className="text-sm text-[#ccc] flex flex-col gap-y-1 items-center">
               <li>
                 <a href="#">React Icons</a>
               </li>
